63258b4bd413c68988397ee0a66994cda5726c09
Benjamin Diaz <benjamin.diaz@upr.edu>
2024-04-25 17:14:18 -0400

    Added subroutines which prints out a supertile

--- Bomberman-CIIC4082/Task4/src/spritecontrol.asm
+++ Bomberman-CIIC4082/Task4/src/spritecontrol.asm
@@ -8,6 +8,16 @@ player_y: .res 1
 frame_position: .res 1
 frame_buffer: .res 1
 dpad: .res 1
+metaIndexX: .res 1
+metaIndexY: .res 1
+highBit: .res 1
+lowBit: .res 1
+level: .res 1
+NTB_offset: .res 1
+mapIndex: .res 1
+
+
+
 
 .exportzp player_x, player_y, frame_position, frame_buffer
 
@@ -44,32 +54,32 @@ dpad: .res 1
 
 .export main
 .proc main
-  ; write a palette
-  LDX PPUSTATUS
-  LDX #$3f
-  STX PPUADDR
-  LDX #$00
-  STX PPUADDR
-load_palettes:
-  LDA palettes,X
-  STA PPUDATA
-  INX
-  CPX #$20
-  BNE load_palettes
+	; write a palette
+	LDX PPUSTATUS
+	LDX #$3f
+	STX PPUADDR
+	LDX #$00
+	STX PPUADDR
+	load_palettes:
+	LDA palettes,X
+	STA PPUDATA
+	INX
+	CPX #$20
+	BNE load_palettes
 
 
-vblankwait:       ; wait for another vblank before continuing
-  BIT PPUSTATUS
-  BPL vblankwait
+	vblankwait:       ; wait for another vblank before continuing
+	BIT PPUSTATUS
+	BPL vblankwait
 
-  LDA #%10010000  ; turn on NMIs, sprites use first pattern table
-  STA PPUCTRL
-  LDA #%00011110  ; turn on screen
-  STA PPUMASK
+	LDA #%10010000  ; turn on NMIs, sprites use first pattern table
+	STA PPUCTRL
+	LDA #%00011110  ; turn on screen
+	STA PPUMASK
 
-forever:
-  JMP forever
-.endproc
+	forever:
+	JMP forever
+	.endproc
 
 
 .proc update_frame
@@ -86,35 +96,35 @@ forever:
 	BEQ next_frame_tile
 	JMP exit_subroutine
 
-next_frame_tile:
+	next_frame_tile:
 
-	LDA frame_position
-	CLC 
-	ADC #$02
-	STA frame_position
+		LDA frame_position
+		CLC 
+		ADC #$02
+		STA frame_position
 
-	LDA frame_position
-	CMP #$06
-	BEQ reset_frame
-	LDA #$00
-	STA frame_buffer
-	JMP exit_subroutine
+		LDA frame_position
+		CMP #$06
+		BEQ reset_frame
+		LDA #$00
+		STA frame_buffer
+		JMP exit_subroutine
 
-reset_frame:
-	LDA #$00
-	STA frame_position
-	STA frame_buffer
+	reset_frame:
+		LDA #$00
+		STA frame_position
+		STA frame_buffer
 
 
-exit_subroutine:
-   ; all done, clean up and return
-  PLA
-  TAY
-  PLA
-  TAX
-  PLA
-  PLP
-  RTS
+	exit_subroutine:
+	; all done, clean up and return
+	PLA
+	TAY
+	PLA
+	TAX
+	PLA
+	PLP
+	RTS
 
 .endproc
 
@@ -579,34 +589,421 @@ exit_subroutine:
 .endproc
 
 .proc read_controller1
- PHA
- TXA
- PHA
- PHP
+	PHA
+	TXA
+	PHA
+	PHP
 
 
- LDA #01
- STA CONTROLLER1 ; controller's shift register set in data collection mode
- LDA #00
- STA CONTROLLER1; sets controller into output mode
+	LDA #01
+	STA CONTROLLER1 ; controller's shift register set in data collection mode
+	LDA #00
+	STA CONTROLLER1; sets controller into output mode
 
- LDA #%00000001
+	LDA #%00000001
 
- STA dpad
+	STA dpad
 
-get_buttons:
-  LDA CONTROLLER1 ; loads the leftmost bit into the accumalator
-  LSR A ; shifts bit0 -> carry
-  ROL dpad ; dpad value <- carry
-  BCC get_buttons
-  PLP
-  PLA
-  TAX
-  PLA
-  RTS
+	get_buttons:
+	LDA CONTROLLER1 ; loads the leftmost bit into the accumalator
+	LSR A ; shifts bit0 -> carry
+	ROL dpad ; dpad value <- carry
+	BCC get_buttons
+	PLP
+	PLA
+	TAX
+	PLA
+	RTS
+.endproc
+
+.proc DecodeMetatile
+	PHP
+	PHA
+	TXA
+	PHA
+	TYA
+	PHA
+
+	decode:
+	
+	LDA level
+	LSR A
+	LSR A 
+	STA metaIndexY
+
+	LDA level
+	AND #%00000011
+	STA metaIndexX
+
+	LDA metaIndexY
+	LSR A
+	LSR A
+	AND #%00000011
+	STA highBit
+
+	LDA metaIndexX
+	ASL A 
+	ASL A 
+	ASL A 
+	
+	LDA metaIndexY
+	ASL A
+	ASL A
+	ASL A
+	ASL A
+	ASL A
+	ASL A
+	
+	CLC 
+	ADC metaIndexX
+	STA lowBit
+
+	PLA
+	TAY
+	PLA
+	TAX
+	PLA
+	PLP
+	RTS
+.endproc
+
+.proc load_brick
+	PHP
+	PHA
+	TXA
+	PHA
+	TYA
+	PHA
+
+	LDA PPUSTATUS
+	LDA highBit
+	CLC
+	ADC NTB_offset
+	STA PPUADDR
+	LDA lowBit
+	STA PPUADDR
+	LDA blocks
+	STA PPUDATA
+
+
+	LDA PPUSTATUS
+	LDA highBit
+	CLC
+	ADC NTB_offset
+	STA PPUADDR
+	LDA lowBit
+	CLC 
+	ADC #$01
+	STA PPUADDR
+	LDA blocks+1
+	STA PPUDATA
+
+	LDA PPUSTATUS
+	LDA highBit
+	CLC
+	ADC NTB_offset
+	STA PPUADDR
+	LDA lowBit
+	CLC 
+	ADC #$20
+	STA PPUADDR
+	LDA blocks+2
+	STA PPUDATA
+
+	LDA PPUSTATUS
+	LDA highBit
+	CLC
+	ADC NTB_offset
+	STA PPUADDR
+	LDA lowBit
+	CLC 
+	ADC #$21
+	STA PPUADDR
+	LDA blocks+3
+	STA PPUDATA
+
+
+	PLA
+	TAY
+	PLA
+	TAX
+	PLA
+	PLP
+	RTS
+.endproc
+
+.proc load_broken_brick
+	PHP
+	PHA
+	TXA
+	PHA
+	TYA
+	PHA
+
+	LDA PPUSTATUS
+	LDA highBit
+	CLC
+	ADC NTB_offset
+	STA PPUADDR
+	LDA lowBit
+	STA PPUADDR
+	LDA blocks+4
+	STA PPUDATA
+
+
+	LDA PPUSTATUS
+	LDA highBit
+	CLC
+	ADC NTB_offset
+	STA PPUADDR
+	LDA lowBit
+	CLC 
+	ADC #$01
+	STA PPUADDR
+	LDA blocks+5
+	STA PPUDATA
+
+	LDA PPUSTATUS
+	LDA highBit
+	CLC
+	ADC NTB_offset
+	STA PPUADDR
+	LDA lowBit
+	CLC 
+	ADC #$20
+	STA PPUADDR
+	LDA blocks+6
+	STA PPUDATA
+
+	LDA PPUSTATUS
+	LDA highBit
+	CLC
+	ADC NTB_offset
+	STA PPUADDR
+	LDA lowBit
+	CLC 
+	ADC #$21
+	STA PPUADDR
+	LDA blocks+7
+	STA PPUDATA
+
+
+	PLA
+	TAY
+	PLA
+	TAX
+	PLA
+	PLP
+	RTS
 .endproc
 
 
+.proc load_grass
+	PHP
+	PHA
+	TXA
+	PHA
+	TYA
+	PHA
+
+	LDA PPUSTATUS
+	LDA highBit
+	CLC
+	ADC NTB_offset
+	STA PPUADDR
+	LDA lowBit
+	STA PPUADDR
+	LDA blocks+8
+	STA PPUDATA
+
+
+	LDA PPUSTATUS
+	LDA highBit
+	CLC
+	ADC NTB_offset
+	STA PPUADDR
+	LDA lowBit
+	CLC 
+	ADC #$01
+	STA PPUADDR
+	LDA blocks+9
+	STA PPUDATA
+
+	LDA PPUSTATUS
+	LDA highBit
+	CLC
+	ADC NTB_offset
+	STA PPUADDR
+	LDA lowBit
+	CLC 
+	ADC #$20
+	STA PPUADDR
+	LDA blocks+10
+	STA PPUDATA
+
+	LDA PPUSTATUS
+	LDA highBit
+	CLC
+	ADC NTB_offset
+	STA PPUADDR
+	LDA lowBit
+	CLC 
+	ADC #$21
+	STA PPUADDR
+	LDA blocks+11
+	STA PPUDATA
+
+
+	PLA
+	TAY
+	PLA
+	TAX
+	PLA
+	PLP
+	RTS
+.endproc
+
+
+.proc load_floor
+	PHP
+	PHA
+	TXA
+	PHA
+	TYA
+	PHA
+
+	LDA PPUSTATUS
+	LDA highBit
+	CLC
+	ADC NTB_offset
+	STA PPUADDR
+	LDA lowBit
+	STA PPUADDR
+	LDA blocks+12
+	STA PPUDATA
+
+
+	LDA PPUSTATUS
+	LDA highBit
+	CLC
+	ADC NTB_offset
+	STA PPUADDR
+	LDA lowBit
+	CLC 
+	ADC #$01
+	STA PPUADDR
+	LDA blocks+12
+	STA PPUDATA
+
+	LDA PPUSTATUS
+	LDA highBit
+	CLC
+	ADC NTB_offset
+	STA PPUADDR
+	LDA lowBit
+	CLC 
+	ADC #$20
+	STA PPUADDR
+	LDA blocks+12
+	STA PPUDATA
+
+	LDA PPUSTATUS
+	LDA highBit
+	CLC
+	ADC NTB_offset
+	STA PPUADDR
+	LDA lowBit
+	CLC 
+	ADC #$21
+	STA PPUADDR
+	LDA blocks+12
+	STA PPUDATA
+
+
+	PLA
+	TAY
+	PLA
+	TAX
+	PLA
+	PLP
+	RTS
+.endproc
+
+
+.proc loadLevel
+	PHP
+	PHA
+	TXA
+	PHA
+	TYA
+	PHA
+
+	LDX #$00
+
+	start:
+	LDA mapIndex
+	AND #%11000000 
+	CMP #%00000000 ; checking if the block is a floor
+	BEQ floor
+	
+	AND #%11000000 
+	CMP #%01000000 ; checking if the block is a brick
+	BEQ brick
+
+	AND #%11000000 
+	CMP #%10000000 ; checking if the block is a broken brick
+	BEQ broken_brick
+
+	AND #%11000000 
+	CMP #%11000000 ; checking if the block is a grass
+	BEQ grass
+
+
+
+
+	
+
+
+	floor: 
+		JSR load_floor
+		JMP blockLoop
+
+	brick: 
+		JSR load_brick
+		JMP blockLoop
+
+	broken_brick: 
+		JSR load_broken_brick
+		JMP blockLoop
+
+	grass: 
+		JSR load_grass
+		JMP blockLoop
+	
+
+
+
+	blockLoop:
+		ASL mapIndex
+		ASL mapIndex
+
+		INX 
+		CPX $04
+		BNE start
+
+
+	PLA
+	TAY
+	PLA
+	TAX
+	PLA
+	PLP
+	RTS
+.endproc
+
+.proc loadWorld1
+
+.endproc
 
 
 .segment "VECTORS"
@@ -632,65 +1029,86 @@ palettes:
 
 
 nametable1_lvl1:
-.byte %01010101, %01010101, %01010101, %01010101
-.byte %01000000, %00000011, %11111111, %00000001
-.byte %01001010, %10100010, %10101010, %00101001
-.byte %01001010, %10100010, %00000000, %00101001
-.byte %01000011, %11100010, %10001010, %10101001
-.byte %01101010, %11101111, %10001000, %00000000
-.byte %01101010, %11101111, %10001000, %00001001
-.byte %01001000, %00000010, %00000010, %00001001
-.byte %01001000, %00101010, %00100010, %00001001
-.byte %01001000, %00100010, %00100010, %00001001
-.byte %01111010, %10100010, %00101110, %10001001
-.byte %01111010, %00000010, %00101111, %00001001
-.byte %01111010, %00101010, %00101110, %10001001
-.byte %01000000, %00000010, %00100010, %10001001
+	.byte %01010101, %01010101, %01010101, %01010101
+	.byte %01000000, %00000011, %11111111, %00000001
+	.byte %01001010, %10100010, %10101010, %00101001
+	.byte %01001010, %10100010, %00000000, %00101001
+
+	.byte %01000011, %11100010, %10001010, %10101001
+	.byte %01101010, %11101111, %10001000, %00000000
+	.byte %01101010, %11101111, %10001000, %00001001
+	.byte %01001000, %00000010, %00000010, %00001001
+
+	.byte %01001000, %00101010, %00100010, %00001001
+	.byte %01001000, %00100010, %00100010, %00001001
+	.byte %01111010, %10100010, %00101110, %10001001
+	.byte %01111010, %00000010, %00101111, %00001001
+
+	.byte %01111010, %00101010, %00101110, %10001001
+	.byte %01000000, %00000010, %00100010, %10001001
 
 nametable2_lvl1:
-.byte %01010101, %01010101, %01010101, %01010101
-.byte %01001000, %00000000, %00000000, %00000001
-.byte %01111011, %10101010, %10101110, %11101001
-.byte %11111111, %11000010, %10101110, %11100001
-.byte %01101010, %10100000, %00101110, %11100001
-.byte %00000000, %00100010, %10100010, %00100001
-.byte %01100010, %00100000, %00100010, %00000001
-.byte %01100010, %00101010, %00100010, %10100001
-.byte %01100010, %00111111, %11100000, %00100001
-.byte %01100010, %00101110, %11101010, %00100001
-.byte %01100010, %00100010, %00100010, %00100001
-.byte %01100010, %00100010, %00100010, %00100001
-.byte %01101110, %10100010, %10100010, %00100000
-.byte %01101111, %11100000, %00000010, %00100001
+	.byte %01010101, %01010101, %01010101, %01010101
+	.byte %01001000, %00000000, %00000000, %00000001
+	.byte %01111011, %10101010, %10101110, %11101001
+	.byte %11111111, %11000010, %10101110, %11100001
+
+	.byte %01101010, %10100000, %00101110, %11100001
+	.byte %00000000, %00100010, %10100010, %00100001
+	.byte %01100010, %00100000, %00100010, %00000001
+	.byte %01100010, %00101010, %00100010, %10100001
+
+	.byte %01100010, %00111111, %11100000, %00100001
+	.byte %01100010, %00101110, %11101010, %00100001
+	.byte %01100010, %00100010, %00100010, %00100001
+	.byte %01100010, %00100010, %00100010, %00100001
+
+	.byte %01101110, %10100010, %10100010, %00100000
+	.byte %01101111, %11100000, %00000010, %00100001
 
 nametable1_lvl2:
-.byte %01010101, %01010101, %01010101, %01010101
-.byte %01000000, %00001010, %10110000, %00000001
-.byte %01001010, %10000011, %11111010, %10101001
-.byte %01001011, %10101011, %10101000, %00000001
-.byte %01001111, %11001011, %11101111, %00000000
-.byte %01101010, %10001010, %11101110, %10101001
-.byte %01000000, %10000010, %00101110, %00000001
-.byte %01001010, %10100010, %00100010, %00000001
-.byte %01001000, %00000010, %00100010, %00101001
-.byte %01001000, %10101010, %00100010, %00100001
-.byte %01001000, %10100000, %00101110, %00100001
-.byte %01001011, %11100000, %00111111, %00000001
-.byte %01001010, %11101010, %10101110, %00100001
-.byte %01000011, %11110000, %00100010, %00100001
+	.byte %01010101, %01010101, %01010101, %01010101
+	.byte %01000000, %00001010, %10110000, %00000001
+	.byte %01001010, %10000011, %11111010, %10101001
+	.byte %01001011, %10101011, %10101000, %00000001
+
+	.byte %01001111, %11001011, %11101111, %00000000
+	.byte %01101010, %10001010, %11101110, %10101001
+	.byte %01000000, %10000010, %00101110, %00000001
+	.byte %01001010, %10100010, %00100010, %00000001
+
+	.byte %01001000, %00000010, %00100010, %00101001
+	.byte %01001000, %10101010, %00100010, %00100001
+	.byte %01001000, %10100000, %00101110, %00100001
+	.byte %01001011, %11100000, %00111111, %00000001
+
+	.byte %01001010, %11101010, %10101110, %00100001
+	.byte %01000011, %11110000, %00100010, %00100001
 
 nametable2_lvl2:
-.byte %01010101, %01010101, %01010101, %01010101
-.byte %01100000, %10000000, %00110000, %00000001
-.byte %01100010, %10001010, %10111010, %10111001
-.byte %01101110, %10001000, %10101000, %10111001
-.byte %00111111, %11001000, %10101000, %10111001
-.byte %01111011, %10001000, %00000000, %10000001
-.byte %01001000, %10001010, %10100000, %10000001
-.byte %01001000, %10001010, %11100010, %10100001
-.byte %01001000, %10001010, %11100000, %00100001
-.byte %01001000, %10000011, %11100010, %10100001
-.byte %01001000, %10101010, %10100010, %10111101
-.byte %01001000, %00000000, %00000010, %10111101
-.byte %01001010, %10101010, %10101010, %10100001
-.byte %01000011, %11111100, %00000000, %00100000
+	.byte %01010101, %01010101, %01010101, %01010101
+	.byte %01100000, %10000000, %00110000, %00000001
+	.byte %01100010, %10001010, %10111010, %10111001
+	.byte %01101110, %10001000, %10101000, %10111001
+
+	.byte %00111111, %11001000, %10101000, %10111001
+	.byte %01111011, %10001000, %00000000, %10000001
+	.byte %01001000, %10001010, %10100000, %10000001
+	.byte %01001000, %10001010, %11100010, %10100001
+
+	.byte %01001000, %10001010, %11100000, %00100001
+	.byte %01001000, %10000011, %11100010, %10100001
+	.byte %01001000, %10101010, %10100010, %10111101
+	.byte %01001000, %00000000, %00000010, %10111101
+
+	.byte %01001010, %10101010, %10101010, %10100001
+	.byte %01000011, %11111100, %00000000, %00100000
+
+
+blocks:
+	.byte $32, $33, $42, $43 ; regular brick block
+	.byte $30, $31, $40, $41 ; broken brick block
+	.byte $34, $35, $44, $45 ; grass block
+	.byte $38, $39, $48, $49 ; floor block
+
+
